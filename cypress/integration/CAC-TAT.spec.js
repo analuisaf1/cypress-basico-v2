7379a444dca09b31eff920faaf3e0c83fb667d76
C1328406 Ana Luisa Floriano dos Santos <c1328406@interno.bb.com.br>
2023-05-22 16:37:25 -0300

    Commit Final

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,6 +1,7 @@
 // <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function () {
+    const THREE_SECS_IN_MS = 3000
     beforeEach(function () {
         cy.visit('./src/index.html')
     })
@@ -11,6 +12,9 @@ describe('Central de Atendimento ao Cliente TAT', function () {
 
     it('preenche os campos obrigatórios e envia o formulário', function () {
         const longText = 'Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste'
+        
+        cy.clock()
+
         cy.get('#firstName').type('Ana Luísa')
         cy.get('#lastName').type('Floriano')
         cy.get('#email').type('ana@exemplo.com')
@@ -18,10 +22,17 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('button[type="submit"]').click()
 
         cy.get('.success').should('be.visible')
+
+        cy.tick(THREE_SECS_IN_MS)
+        
+        cy.get('.success').should('not.be.visible')
     })
 
     it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
         const longText = 'Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste'
+        
+        cy.clock()
+
         cy.get('#firstName').type('Ana Luísa')
         cy.get('#lastName').type('Floriano')
         cy.get('#email').type('anaexemplo.com')
@@ -29,6 +40,8 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('button[type="submit"]').click()
 
         cy.get('.error').should('be.visible')
+
+        cy.tick(THREE_SECS_IN_MS)
     })
 
     it('se um valor não-númerico for digitado no campo telefone, seu valor continuará vazio', function () {
@@ -38,6 +51,8 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     })
 
     it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preechido antes do envio do formulário', function () {
+        cy.clock()
+        
         cy.get('#firstName').type('Ana Luísa')
         cy.get('#lastName').type('Floriano')
         cy.get('#email').type('ana@exemplo.com')
@@ -46,6 +61,10 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('button[type="submit"]').click()
 
         cy.get('.error').should('be.visible')
+
+        cy.tick(THREE_SECS_IN_MS)
+
+        cy.get('.error').should('not.be.visible')
     })
 
     it('preenche e limpa os campos nome, sobrenome, email e telefone', function () {
@@ -72,22 +91,33 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     })
 
     it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function () {
+        cy.clock()
+
         cy.get('button[type="submit"]').click()
 
         cy.get('.error').should('be.visible')
+
+        cy.tick(THREE_SECS_IN_MS)
+
+        cy.get('.error').should('not.be.visible')
     })
 
     it('envia formulário com sucesso usando um comando customizado', function () {
+        cy.clock()
 
         cy.fillMandatoryFieldsAndSubmit()
         cy.get('.success').should('be.visible')
+
+        cy.tick(THREE_SECS_IN_MS)
+
+        cy.get('.error').should('not.be.visible')
     })
 
     it('seleciona um produto (YouTube) por seu texto', function () {
 
         cy.get('#product')
             .select('YouTube')
-            .should('have.value', 'youtube')
+            .should('have.value', 'youtube')       
     })
 
     it('seleciona um produto (Mentoria) por seu valor (value)', function () {
@@ -183,13 +213,53 @@ describe('Central de Atendimento ao Cliente TAT', function () {
             .click()
         cy.contains('Talking About Testing')
             .should('be.visible')
+    })
 
-   })
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', function () {
+        cy.get('.success')
+        .should('not.be.visible')
+        .invoke('show')
+        .should('be.visible')
+        .and('contain', 'Mensagem enviada com sucesso.')
+        .invoke('hide')
+        .should('not.be.visible')
+        cy.get('.error')
+        .should('not.be.visible')
+        .invoke('show')
+        .should('be.visible')
+        .and('contain', 'Valide os campos obrigatórios!')
+        .invoke('hide')
+        .should('not.be.visible')
+    })
+
+    it('preenche a area de texto usando o comando invoke', function () {
+        const longText = Cypress._.repeat('Teste', 20)
+            
+            cy.clock()
+    
+            cy.get('#firstName').type('Ana Luísa')
+            cy.get('#lastName').type('Floriano')
+            cy.get('#email').type('anaexemplo.com')
+            cy.get('#open-text-area').type(longText, { delay: 0 })
+            cy.get('button[type="submit"]').click()
+    
+            cy.get('.error').should('be.visible')
+    
+            cy.tick(THREE_SECS_IN_MS)
+     })
+
+     it('faz uma requisição HTTP', function () {
+
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .should(function(response) {
+                 const { status, statusText, body } = response
+                 expect(status).to.equal(200)
+                 expect(statusText).to.equal('OK')
+                 expect(body).to.include('CAC TAT')
+
+            })
+
+    })
+
 
 })
-
-
-
-
-
-
--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,8 +1,11 @@
-it.only('testa a página da política de privacidade de forma independente', function () {
-    cy.visit('./src/privacy.html')
+Cypress._.times(5, function(){
 
-    cy.contains('Talking About Testing')
-    .should('be.visible')
-
-
+    it.only('testa a página da política de privacidade de forma independente', function () {
+        cy.visit('./src/privacy.html')
+    
+        cy.contains('Talking About Testing')
+        .should('be.visible')
+    
+    
+    })
 })
